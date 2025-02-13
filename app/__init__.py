d64876840e53fb581ea4dfa611a90d836a413fb0
Samuel Williams <samuelhwilliams@gmail.com>
2025-02-13 16:39:48 +0000

    add flask-debugtoolbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,18 @@
 from flask import Flask
 
-from app.extensions import db, migrate
+from app.extensions import db, migrate, toolbar
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
 
+    db.init_app(app)
+    migrate.init_app(app, db, directory='app/common/data/migrations', compare_type=True, render_as_batch=True)
+    toolbar.init_app(app)
+
     from app.healthcheck import healthcheck_blueprint
 
     app.register_blueprint(healthcheck_blueprint)
-
-    db.init_app(app)
-    migrate.init_app(app, db, directory='app/common/data/migrations', compare_type=True, render_as_batch=True)
 
     return app
--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,4 +1,7 @@
+# TODO: all of this
 class Config:
     SQLALCHEMY_ENGINES = {
         "default": "sqlite:///default.sqlite"
     }
+    SECRET_KEY = 'unsafe'
+    DEBUG_TB_ENABLED = True
