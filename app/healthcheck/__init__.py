8486443e63ddddcad890d6bca6e269126363bb15
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2025-02-13 14:08:55 +0000

    add pre-commit

--- app/healthcheck/__init__.py
+++ app/healthcheck/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
-healthcheck_blueprint = Blueprint(name='healthcheck', import_name=__name__)
 
-@healthcheck_blueprint.route('/healthcheck')
+healthcheck_blueprint = Blueprint(name="healthcheck", import_name=__name__)
+
+
+@healthcheck_blueprint.route("/healthcheck")
 def healthcheck():
-    return 'OK'
+    return "OK"
