4b61e31077d31873b2cf58cbfa50a693050ecdb5
Samuel Williams <samuelhwilliams@gmail.com>
2025-02-17 17:35:01 +0000

    basic plaintext/json logging

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,14 +1,24 @@
 # TODO: all of this
 import os
+from typing import Literal
 
 
 class Config:
+    # Flask app
+    SERVER_NAME = "funding.communities.gov.localhost:8080"
+    SECRET_KEY = "unsafe"  # pragma: allowlist secret
+
+    # Databases
     SQLALCHEMY_ENGINES = {
         "default": os.environ.get(
             "DATABASE_URL",
             "postgresql+psycopg://postgres:postgres@localhost:5432/postgres",
         )
     }
-    SERVER_NAME = "funding.communities.gov.localhost:8080"
-    SECRET_KEY = "unsafe"  # pragma: allowlist secret
+
+    # Logging
+    LOG_LEVEL = "INFO"
+    LOG_FORMATTER: Literal["plaintext", "json"] = "json"
+
+    # Flask-DebugToolbar
     DEBUG_TB_ENABLED = True
