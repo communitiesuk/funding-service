d265be95e5ef75a7ecea38f5ab6ac5bc3aa5eccd
srh-sloan <sarah.sloan@communities.gov.uk>
2025-04-04 12:43:29 +0100

    Automatically excluding e2e or non e2e tests from a pytest run, depending on the --e2e argument Updated the README accordingly

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,32 @@
-from typing import Generator
+from typing import Any, Generator
 
 import pytest
+from _pytest.config import Config
+from _pytest.config.argparsing import Parser
 from flask import Flask
 
 from app import create_app
+
+
+def pytest_addoption(parser: Parser) -> None:
+    parser.addoption("--e2e", action="store_true", default=False, help="run e2e (browser) tests")
+
+
+def pytest_collection_modifyitems(config: Config, items: list[Any]) -> None:
+    # Determines whether e2e tests have been requested. If not, skips anything marked as e2e.
+    # If e2e tests are requested, skips everything not marked as e2e
+    skip_e2e = pytest.mark.skip(reason="only running non-e2e tests")
+    skip_non_e2e = pytest.mark.skip(reason="only running e2e tests")
+
+    e2e_run = config.getoption("--e2e")
+    if e2e_run:
+        for item in items:
+            if "e2e" not in item.keywords:
+                item.add_marker(skip_non_e2e)
+    else:
+        for item in items:
+            if "e2e" in item.keywords:
+                item.add_marker(skip_e2e)
 
 
 @pytest.fixture(scope="session")
