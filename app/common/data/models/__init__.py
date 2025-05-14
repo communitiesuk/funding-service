de3b894190ac72eba3da3bb167113dde034012c3
Steven <steven.fountain@communities.gov.uk>
2025-05-14 20:22:50 +0100

    WIP: models based on current PRs, will match with my nested concept

--- app/common/data/models/__init__.py
+++ app/common/data/models/__init__.py
@@ -1,9 +1,11 @@
 import datetime
 import secrets
 import uuid
+from typing import Optional
 
 from pytz import utc
 from sqlalchemy import ForeignKey, Index, UniqueConstraint
+from sqlalchemy.ext.orderinglist import ordering_list
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.common.data.base import BaseModel, CIStr
@@ -74,4 +76,77 @@ class CollectionSchema(BaseModel):
     created_by_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"))
     created_by: Mapped[User] = relationship("User")
 
+    sections: Mapped[list["Section"]] = relationship(
+        "Section", lazy=True, order_by="Section.order", collection_class=ordering_list("order", count_from=1)
+    )
+
     __table_args__ = (UniqueConstraint("name", "grant_id", "version", name="uq_schema_name_version_grant_id"),)
+
+
+class Section(BaseModel):
+    __tablename__ = "section"
+
+    title: Mapped[str]
+    order: Mapped[int]
+
+    collection_schema_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("collection_schema.id"))
+    collection_schema: Mapped[CollectionSchema] = relationship("CollectionSchema", back_populates="sections")
+
+    forms: Mapped[list["Form"]] = relationship(
+        "Form", order_by="Form.order", collection_class=ordering_list("order", count_from=1)
+    )
+
+    __table_args__ = (
+        UniqueConstraint("order", "collection_schema_id", name="uq_section_order_collection_schema", deferrable=True),
+        UniqueConstraint("title", "collection_schema_id", name="uq_section_title_collection_schema"),
+    )
+
+
+class Form(BaseModel):
+    __tablename__ = "form"
+
+    title: Mapped[str]
+    order: Mapped[int]
+    slug: Mapped[str]
+
+    section_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("section.id"))
+    section: Mapped[Section] = relationship("Section", back_populates="forms")
+
+    # this is denormalised intentionally to allow for a unique constraint on the parent table
+    # having sections not be a heirarchical element would allow for this in a cleaner way I think
+    collection_schema_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("collection_schema.id"))
+
+    __table_args__ = (
+        UniqueConstraint("order", "section_id", name="uq_form_order_section", deferrable=True),
+        UniqueConstraint("title", "collection_schema_id", name="uq_form_title_collection_schema"),
+        UniqueConstraint("slug", "section_id", name="uq_form_slug_section"),
+    )
+
+    questions: Mapped[list["Question"]] = relationship(
+        "Question", lazy=True, order_by="Question.order", collection_class=ordering_list("order", count_from=1)
+    )
+
+    sections: Mapped[list["Section"]] = relationship(
+        "Section", lazy=True, order_by="Section.order", collection_class=ordering_list("order", count_from=1)
+    )
+
+
+class Question(BaseModel):
+    __tablename__ = "question"
+
+    text: Mapped[str]
+    slug: Mapped[str]
+    order: Mapped[int]
+    hint: Mapped[Optional[str]]
+    name: Mapped[str]
+
+    data_type: Mapped[str]  # TODO this will be an enum in proper data model
+
+    form_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("form.id"))
+    form: Mapped[Form] = relationship("Form", back_populates="questions")
+
+    __table_args__ = (
+        UniqueConstraint("order", "form_id", name="uq_question_order_form", deferrable=True),
+        UniqueConstraint("slug", "form_id", name="uq_question_slug_form"),
+        UniqueConstraint("name", "form_id", name="uq_question_name_form"),
+    )
