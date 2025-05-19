e280a2995aafd80f4da71e6703f292acbf28ddf4
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-05-19 11:21:56 +0100

    Get update from fspt-406-manage-forms

--- app/common/data/types.py
+++ app/common/data/types.py
@@ -1,1 +1,20 @@
+import enum
+
 json_scalars = str | int | float | bool
+
+
+class QuestionType(enum.Enum):
+    QUESTION = "QUESTION"
+    INFORMATION = "INFORMATION"
+    GROUP = "GROUP"
+
+
+class DataType(enum.Enum):
+    TEXT = "TEXT"
+    INT = "INT"
+    CONTACT_DETAILS = "CONTACT_DETAILS"
+
+
+class ConditionType(enum.Enum):
+    ANSWER_EQUALS = "="
+    ANSWER_NOT_EQUALS = "!="
