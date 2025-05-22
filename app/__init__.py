1cd4767b31c9b1bbd9f8898eb21ac925653e0859
srh-sloan <sarah.sloan@communities.gov.uk>
2025-05-22 11:48:18 +0100

    Renaming app/deliver_grant_funding/__init__.py -> app/deliver_grant_funding/routes.py and associated import changes

--- app/__init__.py
+++ app/__init__.py
@@ -121,7 +121,7 @@ def create_app() -> Flask:
 
     # Attach routes
     from app.common.auth import auth_blueprint
-    from app.deliver_grant_funding import deliver_grant_funding_blueprint
+    from app.deliver_grant_funding.routes import deliver_grant_funding_blueprint
     from app.healthcheck import healthcheck_blueprint
 
     app.register_blueprint(healthcheck_blueprint)
