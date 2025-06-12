104d8d01a8db171c4a27997da2abed69c86f1e7a
srh-sloan <sarah.sloan@communities.gov.uk>
2025-06-12 13:27:20 +0100

    Basic sso flow using real ms pages

--- tests/e2e/test_sso_auth.py
+++ tests/e2e/test_sso_auth.py
@@ -1,11 +1,13 @@
+from os import getenv
+
 import pytest
 from playwright.sync_api import Page, expect
 
-from tests.e2e.pages import SSOSignInPage, StubSSOEmailLoginPage
+from tests.e2e.pages import MicrosoftLoginPageEmail, SSOSignInPage, StubSSOEmailLoginPage
 
 
 @pytest.mark.skip_in_environments(["dev", "test", "prod"])
-def test_sso_journey(page: Page, domain: str):
+def test_stub_sso_journey(page: Page, domain: str):
     sso_sign_in_page = SSOSignInPage(page, domain)
     sso_sign_in_page.navigate()
     sso_sign_in_page.click_sign_in()
@@ -13,5 +15,22 @@ def test_sso_journey(page: Page, domain: str):
     sso_email_login_page = StubSSOEmailLoginPage(page, domain)
     sso_email_login_page.fill_email_address("test@communities.gov.uk")
     sso_email_login_page.click_sign_in()
+
+    expect(page).to_have_url(f"{domain}/grants")
+
+
+def test_real_sso_journey(page: Page, domain: str):
+    sign_in_email = getenv("SERVICE_ACCOUNT_USERNAME", None)
+    sign_in_password = getenv("SERVICE_ACCOUNT_PASSWORD", None)
+    if not sign_in_email or not sign_in_password:
+        pytest.fail("SERVICE_ACCOUNT_USERNAME and SERVICE_ACCOUNT_PASSWORD must be set for this test")
+    sso_sign_in_page = SSOSignInPage(page, domain)
+    sso_sign_in_page.navigate()
+    sso_sign_in_page.click_sign_in()
+    ms_email_page = MicrosoftLoginPageEmail(page, domain, sign_in_email, sign_in_password)
+    ms_email_page.fill_email_address()
+    ms_password_page = ms_email_page.click_next()
+    ms_password_page.fill_password()
+    ms_password_page.click_sign_in()
 
     expect(page).to_have_url(f"{domain}/grants")
