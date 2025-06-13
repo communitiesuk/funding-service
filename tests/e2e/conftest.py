6141c9fc96fb2cd4ca6105209ddff74364acbc5e
srh-sloan <sarah.sloan@communities.gov.uk>
2025-06-13 11:28:55 +0100

    trying tee

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -30,7 +30,6 @@ def domain(request: pytest.FixtureRequest, get_e2e_params: dict[str, str]) -> st
     e2e_env = get_e2e_params["e2e_env"]
 
     if e2e_env == "local":
-        # return "http://localhost:8080"
         return "https://funding.communities.gov.localhost:8080"
     if e2e_env == "dev":
         return "https://funding.dev.communities.gov.uk"
