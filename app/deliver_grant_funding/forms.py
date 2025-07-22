6e7bdc59b09890bdb88a56d655774ed5dd07310f
Samuel Williams <samuel.williams@communities.gov.uk>
2025-07-22 09:11:38 +0100

    Limit radio questions to 100 items
    We want to limit the number of data source items just to constrain performance
    and limit any kind of denial-of-service/unexpected use.
    
    However, we should expect to raise this as legitimate uses for more than 100
    options exist (eg lists of organisations or local authorities).

--- app/deliver_grant_funding/forms.py
+++ app/deliver_grant_funding/forms.py
@@ -1,6 +1,7 @@
-from typing import Any
+from typing import Any, Callable
 from uuid import UUID
 
+from flask import current_app
 from flask_wtf import FlaskForm
 from govuk_frontend_wtf.wtforms_widgets import (
     GovCharacterCount,
@@ -35,6 +36,14 @@ def _validate_no_duplicates(form: FlaskForm, field: Field) -> None:
     choices = [choice.strip() for choice in field.data.split("\n")]
     if len(choices) != len(set(choices)):
         raise ValidationError("Remove duplicate options from the list")
+
+
+def _validate_max_list_length(max_length: int) -> Callable[[Any, Any], None]:
+    def validator(form: FlaskForm, field: Field) -> None:
+        if len(field.data.split("\n")) > max_length:
+            raise ValidationError(f"You have entered too many options. The maximum is {max_length}")
+
+    return validator
 
 
 class GrantSetupForm(FlaskForm):
@@ -241,6 +250,7 @@ class QuestionForm(FlaskForm):
                 DataRequired("Enter the options for your list"),
                 _validate_no_blank_lines,
                 _validate_no_duplicates,
+                _validate_max_list_length(max_length=current_app.config["MAX_DATA_SOURCE_ITEMS"]),
             ]
 
 
