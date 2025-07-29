29e27eaf35bc1eaf8923f89c807649ab36fe6226
Samuel Williams <samuel.williams@communities.gov.uk>
2025-07-29 07:23:49 +0100

    FSPT-720 create monitoring reports
    Adds a page to the 'Reports' tab where grant admins and above can create a
    monitoring report for the current grant.

--- app/deliver_grant_funding/routes/reports.py
+++ app/deliver_grant_funding/routes/reports.py
@@ -1,12 +1,17 @@
 from uuid import UUID
 
-from flask import render_template
+from flask import redirect, render_template, url_for
 from flask.typing import ResponseReturnValue
 
 from app.common.auth.decorators import has_grant_role
+from app.common.data import interfaces
+from app.common.data.interfaces.collections import create_collection
+from app.common.data.interfaces.exceptions import DuplicateValueError
 from app.common.data.interfaces.grants import get_grant
-from app.common.data.types import RoleEnum
+from app.common.data.types import CollectionType, RoleEnum
+from app.deliver_grant_funding.forms import SetUpReportForm
 from app.deliver_grant_funding.routes import deliver_grant_funding_blueprint
+from app.extensions import auto_commit_after_request
 
 
 @deliver_grant_funding_blueprint.route("/grant/<uuid:grant_id>/reports", methods=["GET"])
@@ -14,3 +19,27 @@ from app.deliver_grant_funding.routes import deliver_grant_funding_blueprint
 def list_reports(grant_id: UUID) -> ResponseReturnValue:
     grant = get_grant(grant_id, with_all_collections=True)
     return render_template("deliver_grant_funding/reports/list_reports.html", grant=grant)
+
+
+@deliver_grant_funding_blueprint.route("/grant/<uuid:grant_id>/set-up-report", methods=["GET", "POST"])
+@has_grant_role(RoleEnum.ADMIN)
+@auto_commit_after_request
+def set_up_report(grant_id: UUID) -> ResponseReturnValue:
+    grant = get_grant(grant_id)
+    form = SetUpReportForm()
+    if form.validate_on_submit():
+        assert form.name.data
+        try:
+            create_collection(
+                name=form.name.data,
+                user=interfaces.user.get_current_user(),
+                grant=grant,
+                type_=CollectionType.MONITORING_REPORT,
+            )
+            # TODO: Redirect to the 'view collection' page when we've added it.
+            return redirect(url_for("deliver_grant_funding.list_reports", grant_id=grant_id))
+
+        except DuplicateValueError:
+            form.name.errors.append("A report with this name already exists")  # type: ignore[attr-defined]
+
+    return render_template("deliver_grant_funding/reports/set_up_report.html", grant=grant, form=form)
--- tests/unit/test_all_routes_access.py
+++ tests/unit/test_all_routes_access.py
@@ -63,6 +63,7 @@ routes_with_expected_grant_admin_only_access = [
     "deliver_grant_funding.grant_change_name",
     "deliver_grant_funding.grant_change_description",
     "deliver_grant_funding.grant_change_contact",
+    "deliver_grant_funding.set_up_report",
 ]
 routes_with_expected_member_only_access = [
     "deliver_grant_funding.list_users_for_grant",
