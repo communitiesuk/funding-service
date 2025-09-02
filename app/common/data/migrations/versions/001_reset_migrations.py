a29bb9c654d0f4cf995087bf56f6a7405353216e
Samuel Williams <samuel.williams@communities.gov.uk>
2025-09-02 12:58:01 +0100

    Prepare to squash all migrations

---
+++ app/common/data/migrations/versions/001_reset_migrations.py
@@ -0,0 +1,20 @@
+"""reset migrations
+
+Revision ID: 001_reset_migrations
+Revises: 035_remove_sections
+Create Date: 2025-09-02 12:57:53.427819
+
+"""
+
+revision = "001_reset_migrations"
+down_revision = "035_remove_sections"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
