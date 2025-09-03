48abef28b5ac536aaea4c48b6f5f6f8a5fc43bf0
srh-sloan <sarah.sloan@communities.gov.uk>
2025-09-03 15:42:37 +0100

    FSPT-770 First pass implementation of a date type question. Accepts the date using only numbers (eg. 01 03 2024), basic validation (data required) and no expressions.

---
+++ app/common/data/migrations/versions/002_add_date_type.py
@@ -0,0 +1,45 @@
+"""Add a new question type of Date
+
+Revision ID: 002_add_date_type
+Revises: 001_reset_migrations
+Create Date: 2025-09-04 12:29:37.196441
+
+"""
+
+from alembic import op
+from alembic_postgresql_enum import TableReference
+
+revision = "002_add_date_type"
+down_revision = "001_reset_migrations"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.sync_enum_values(
+        enum_schema="public",
+        enum_name="question_data_type_enum",
+        new_values=[
+            "TEXT_SINGLE_LINE",
+            "TEXT_MULTI_LINE",
+            "EMAIL",
+            "URL",
+            "INTEGER",
+            "YES_NO",
+            "RADIOS",
+            "CHECKBOXES",
+            "DATE",
+        ],
+        affected_columns=[TableReference(table_schema="public", table_name="component", column_name="data_type")],
+        enum_values_to_rename=[],
+    )
+
+
+def downgrade() -> None:
+    op.sync_enum_values(
+        enum_schema="public",
+        enum_name="question_data_type_enum",
+        new_values=["TEXT_SINGLE_LINE", "TEXT_MULTI_LINE", "EMAIL", "URL", "INTEGER", "YES_NO", "RADIOS", "CHECKBOXES"],
+        affected_columns=[TableReference(table_schema="public", table_name="component", column_name="data_type")],
+        enum_values_to_rename=[],
+    )
