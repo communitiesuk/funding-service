287ef09c329e4061cd7caa0dc9b23c8eccc98fa0
Samuel Williams <samuel.williams@communities.gov.uk>
2025-09-19 10:58:26 +0100

    Update preview guidance API endpoint to interpolate
    When rendering guidance markdown to HTML, let's also interpolate the markdown so
    that context can be injected for previewing.

--- app/deliver_grant_funding/routes/api.py
+++ app/deliver_grant_funding/routes/api.py
@@ -5,7 +5,9 @@ from flask import Blueprint, current_app, jsonify
 from flask.typing import ResponseReturnValue
 
 from app.common.auth.authorisation_helper import AuthorisationHelper
+from app.common.data.interfaces.collections import get_collection
 from app.common.data.interfaces.user import get_current_user
+from app.common.helpers.collections import SubmissionHelper
 from app.deliver_grant_funding.forms import PreviewGuidanceForm
 from app.deliver_grant_funding.types import (
     PreviewGuidanceBadRequestResponse,
@@ -16,19 +18,21 @@ from app.deliver_grant_funding.types import (
 deliver_grant_funding_api_blueprint = Blueprint("api", __name__)
 
 
-@deliver_grant_funding_api_blueprint.post("/api/v1/<uuid:grant_id>/preview-guidance")
-def preview_guidance(grant_id: UUID) -> ResponseReturnValue:
+@deliver_grant_funding_api_blueprint.post("/api/v1/<uuid:collection_id>/preview-guidance")
+def preview_guidance(collection_id: UUID) -> ResponseReturnValue:
     if not AuthorisationHelper.is_deliver_grant_funding_user(get_current_user()):
         return jsonify(PreviewGuidanceUnauthorisedResponse().model_dump(mode="json")), 401
 
-    if not AuthorisationHelper.is_grant_member(grant_id, get_current_user()):
+    collection = get_collection(collection_id)
+    if not AuthorisationHelper.is_grant_member(collection.grant_id, get_current_user()):
         return jsonify(PreviewGuidanceUnauthorisedResponse().model_dump(mode="json")), 401
 
     form = PreviewGuidanceForm()
     if form.validate_on_submit():
+        interpolate = SubmissionHelper.get_interpolator(collection, fallback_question_names=True)
         return jsonify(
             PreviewGuidanceSuccessResponse(
-                guidance_html=current_app.extensions["govuk_markdown"].convert(form.guidance.data)
+                guidance_html=interpolate(current_app.extensions["govuk_markdown"].convert(form.guidance.data))
             ).model_dump(mode="json")
         ), 200
 
--- tests/integration/deliver_grant_funding/routes/test_api_routes.py
+++ tests/integration/deliver_grant_funding/routes/test_api_routes.py
@@ -7,20 +7,21 @@ from app.deliver_grant_funding.forms import PreviewGuidanceForm
 class TestPreviewGuidance:
     @pytest.mark.authenticate_as("person@gmail.com")
     def test_post_unauthorised_if_not_mhclg_email_address(self, authenticated_no_role_client, factories):
-        grant = factories.grant.create()
+        collection = factories.collection.create()
 
         form = PreviewGuidanceForm(markdown="")
         response = authenticated_no_role_client.post(
-            url_for("deliver_grant_funding.api.preview_guidance", grant_id=grant.id), json=form.data
+            url_for("deliver_grant_funding.api.preview_guidance", collection_id=collection.id), json=form.data
         )
         assert response.status_code == 401
         assert response.json["error"] == "Unauthorised"
 
     @pytest.mark.authenticate_as("person@communities.gov.uk")
-    def test_post_success_with_mhclg_user(self, authenticated_grant_member_client):
+    def test_post_success_with_mhclg_user(self, authenticated_grant_member_client, factories):
+        collection = factories.collection.create(grant=authenticated_grant_member_client.grant)
         form = PreviewGuidanceForm(guidance="## Heading\n\n* list item\n\n[link](https://www.gov.uk)")
         response = authenticated_grant_member_client.post(
-            url_for("deliver_grant_funding.api.preview_guidance", grant_id=authenticated_grant_member_client.grant.id),
+            url_for("deliver_grant_funding.api.preview_guidance", collection_id=collection.id),
             json=form.data,
         )
         assert response.status_code == 200
@@ -33,3 +34,15 @@ class TestPreviewGuidance:
             '<a href="https://www.gov.uk" class="govuk-link govuk-link--no-visited-state">link</a>'
             "</p>\n"
         )
+
+    @pytest.mark.authenticate_as("person@communities.gov.uk")
+    def test_post_interpolates_guidance(self, authenticated_grant_member_client, factories):
+        collection = factories.collection.create(grant=authenticated_grant_member_client.grant)
+        q = factories.question.create(form__collection=collection, name="my question name")
+        form = PreviewGuidanceForm(guidance=f"Test interpolation: (({q.safe_qid}))")
+        response = authenticated_grant_member_client.post(
+            url_for("deliver_grant_funding.api.preview_guidance", collection_id=collection.id),
+            json=form.data,
+        )
+        assert response.status_code == 200
+        assert response.json["guidance_html"] == "<p class='govuk-body'>Test interpolation: ((my question name))</p>\n"
