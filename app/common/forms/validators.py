uk-gov-mirror/communitiesuk.funding-service

FSPT-960: Update magic link perms checks & redirect
Magic link sign in route now enforces a check on the email requesting
the link to have a user account and permissions for Access grant funding
(ie. they have a user_role for a grant_recipient organisation).

Now that we have some initial Access grant funding routes, we can
also update the default redirect path for users signing in via
magic link if they have no onward redirect path.

app/common/forms/validators.py
@@ -9,6 +9,8 @@ from wtforms.fields.core import Field
 from wtforms.form import BaseForm
 from wtforms.validators import Email, HostnameValidation, Regexp, ValidationError
 
+from app.common.data import interfaces
+
 
 class WordRange:
     """
@@ -74,6 +76,26 @@ class CommunitiesEmail(Email):
 
         if allowed_domains and domain.lower() not in [d.lower() for d in allowed_domains]:
             raise ValidationError(f"Email address must end with {' or '.join(allowed_domains)}")
+
+
+class AccessGrantFundingEmail(Email):
+    def __call__(self, form: BaseForm, field: Field) -> None:
+        email = field.data
+        internal_domains = current_app.config["INTERNAL_DOMAINS"]
+
+        if not email:
+            return
+
+        if email.endswith(internal_domains):
+            return
+
+        user = interfaces.user.get_user_by_email(email)
+
+        if user is None or not user.grant_recipients():
+            raise ValidationError(
+                "The email address you entered does not have access to this service. "
+                "Check the email address is correct or request access."
+            )
 
 
 class URLWithoutProtocol(Regexp):

tests/integration/common/auth/test_auth.py
@@ -10,6 +10,7 @@ from app.common.auth.authorisation_helper import AuthorisationHelper
 from app.common.data import interfaces
 from app.common.data.models_user import Invitation, MagicLink, User, UserRole
 from app.common.data.types import RoleEnum
+from tests.models import _get_grant_managing_organisation
 from tests.utils import AnyStringMatching, get_h1_text, get_h2_text, page_has_error
 
 
@@ -43,15 +44,43 @@ class TestMagicLinkSignInView:
         with anonymous_client.session_transaction() as session:
             assert "magic_link_redirect" not in session
 
-    def test_post_valid_non_mhclg_email(self, anonymous_client, mock_notification_service_calls):
+    def test_post_invalid_non_mhclg_email(self, anonymous_client, factories, mock_notification_service_calls):
+        response = anonymous_client.post(
+            url_for("auth.request_a_link_to_sign_in"), data={"email_address": "test@localgov.gov.uk"}
+        )
+
+        assert response.status_code == 200
+        soup = BeautifulSoup(response.data, "html.parser")
+        assert page_has_error(
+            soup,
+            (
+                "The email address you entered does not have access to this service. "
+                "Check the email address is correct or request access."
+            ),
+        )
+
+    def test_post_valid_non_mhclg_email(self, anonymous_client, factories, mock_notification_service_calls):
+        recipient_org = factories.organisation.create(can_manage_grants=False)
+        grant_org = _get_grant_managing_organisation()
+        grant = factories.grant.create(organisation=grant_org)
+        grant_recipient = factories.grant_recipient.create(grant=grant, organisation=recipient_org)
+        user = factories.user.create(email="test@localgov.gov.uk")
+        factories.user_role.create(
+            user=user, organisation=recipient_org, grant=grant, permissions=[RoleEnum.DATA_PROVIDER]
+        )
+        user._grant_recipients = [grant_recipient]
+
         response = anonymous_client.post(
             url_for("auth.request_a_link_to_sign_in"),
-            data={"email_address": "test@example.com"},
+            data={"email_address": user.email},
             follow_redirects=True,
         )
         assert response.status_code == 200
-        assert b"Check your email" in response.data
-        assert b"test@example.com" in response.data
+        soup = BeautifulSoup(response.data, "html.parser")
+
+        assert "Check your email" in get_h1_text(soup)
+        assert "test@localgov.gov.uk" in soup.text
+
         assert len(mock_notification_service_calls) == 1
         assert mock_notification_service_calls[0].kwargs["personalisation"]["magic_link"] == AnyStringMatching(
             r"http://funding.communities.gov.localhost:8080/sign-in/.*"
@@ -63,25 +92,59 @@ class TestMagicLinkSignInView:
 
     @pytest.mark.parametrize(
         "next_, safe_next",
-        (
-            ("/blah/blah", "/blah/blah"),
-            ("https://bad.place/blah", "/"),  # Single test case; see TestSanitiseRedirectURL for more exhaustion
-        ),
+        (("/blah/blah", "/blah/blah"), ("https://bad.place/blah", "/")),
     )
     def test_post_valid_email_with_redirect(
-        self, anonymous_client, mock_notification_service_calls, db_session, next_, safe_next
+        self, anonymous_client, mock_notification_service_calls, factories, db_session, next_, safe_next
     ):
+        recipient_org = factories.organisation.create(can_manage_grants=False)
+        grant_org = _get_grant_managing_organisation()
+        grant = factories.grant.create(organisation=grant_org)
+        grant_recipient = factories.grant_recipient.create(grant=grant, organisation=recipient_org)
+        user = factories.user.create(email="test@localgov.gov.uk")
+        factories.user_role.create(
+            user=user, organisation=recipient_org, grant=grant, permissions=[RoleEnum.DATA_PROVIDER]
+        )
+        user._grant_recipients = [grant_recipient]
+
         with anonymous_client.session_transaction() as session:
             session["next"] = next_
 
         response = anonymous_client.post(
             url_for("auth.request_a_link_to_sign_in"),
-            data={"email_address": "test@example.com"},
+            data={"email_address": user.email},
             follow_redirects=True,
         )
         assert response.status_code == 200
         assert (
             db_session.scalar(select(MagicLink).order_by(MagicLink.created_at_utc.desc())).redirect_to_path == safe_next
+        )
+
+        with anonymous_client.session_transaction() as session:
+            assert "next" not in session
+
+    def test_post_valid_email_with_no_next_redirects_to_route(
+        self, anonymous_client, mock_notification_service_calls, factories, db_session
+    ):
+        recipient_org = factories.organisation.create(can_manage_grants=False)
+        grant_org = _get_grant_managing_organisation()
+        grant = factories.grant.create(organisation=grant_org)
+        grant_recipient = factories.grant_recipient.create(grant=grant, organisation=recipient_org)
+        user = factories.user.create(email="test@localgov.gov.uk")
+        factories.user_role.create(
+            user=user, organisation=recipient_org, grant=grant, permissions=[RoleEnum.DATA_PROVIDER]
+        )
+        user._grant_recipients = [grant_recipient]
+
+        response = anonymous_client.post(
+            url_for("auth.request_a_link_to_sign_in"),
+            data={"email_address": user.email},
+            follow_redirects=True,
+        )
+        assert response.status_code == 200
+        assert (
+            db_session.scalar(select(MagicLink).order_by(MagicLink.created_at_utc.desc())).redirect_to_path
+            == "/access/"
         )
 
         with anonymous_client.session_transaction() as session:

communitiesuk/funding-service

app/common/forms/validators.py
@@ -9,6 +9,8 @@ from wtforms.fields.core import Field
 from wtforms.form import BaseForm
 from wtforms.validators import Email, HostnameValidation, Regexp, ValidationError
 
+from app.common.data import interfaces
+
 
 class WordRange:
     """
@@ -74,6 +76,26 @@ class CommunitiesEmail(Email):
 
         if allowed_domains and domain.lower() not in [d.lower() for d in allowed_domains]:
             raise ValidationError(f"Email address must end with {' or '.join(allowed_domains)}")
+
+
+class AccessGrantFundingEmail(Email):
+    def __call__(self, form: BaseForm, field: Field) -> None:
+        email = field.data
+        internal_domains = current_app.config["INTERNAL_DOMAINS"]
+
+        if not email:
+            return
+
+        if email.endswith(internal_domains):
+            return
+
+        user = interfaces.user.get_user_by_email(email)
+
+        if user is None or not user.get_grant_recipients():
+            raise ValidationError(
+                "The email address you entered does not have access to this service. "
+                "Check the email address is correct or request access."
+            )
 
 
 class URLWithoutProtocol(Regexp):

tests/integration/common/auth/test_auth.py
@@ -10,6 +10,7 @@ from app.common.auth.authorisation_helper import AuthorisationHelper
 from app.common.data import interfaces
 from app.common.data.models_user import Invitation, MagicLink, User, UserRole
 from app.common.data.types import RoleEnum
+from tests.models import _get_grant_managing_organisation
 from tests.utils import AnyStringMatching, get_h1_text, get_h2_text, page_has_error
 
 
@@ -43,15 +44,42 @@ class TestMagicLinkSignInView:
         with anonymous_client.session_transaction() as session:
             assert "magic_link_redirect" not in session
 
-    def test_post_valid_non_mhclg_email(self, anonymous_client, mock_notification_service_calls):
+    def test_post_invalid_non_mhclg_email(self, anonymous_client, factories, mock_notification_service_calls):
+        response = anonymous_client.post(
+            url_for("auth.request_a_link_to_sign_in"), data={"email_address": "test@localgov.gov.uk"}
+        )
+
+        assert response.status_code == 200
+        soup = BeautifulSoup(response.data, "html.parser")
+        assert page_has_error(
+            soup,
+            (
+                "The email address you entered does not have access to this service. "
+                "Check the email address is correct or request access."
+            ),
+        )
+
+    def test_post_valid_non_mhclg_email(self, anonymous_client, factories, mock_notification_service_calls):
+        recipient_org = factories.organisation.create(can_manage_grants=False)
+        mhclg = _get_grant_managing_organisation()
+        grant = factories.grant.create(organisation=mhclg)
+        factories.grant_recipient.create(grant=grant, organisation=recipient_org)
+        user = factories.user.create(email="test@localgov.gov.uk")
+        factories.user_role.create(
+            user=user, organisation=recipient_org, grant=grant, permissions=[RoleEnum.DATA_PROVIDER]
+        )
+
         response = anonymous_client.post(
             url_for("auth.request_a_link_to_sign_in"),
-            data={"email_address": "test@example.com"},
+            data={"email_address": user.email},
             follow_redirects=True,
         )
         assert response.status_code == 200
-        assert b"Check your email" in response.data
-        assert b"test@example.com" in response.data
+        soup = BeautifulSoup(response.data, "html.parser")
+
+        assert "Check your email" in get_h1_text(soup)
+        assert "test@localgov.gov.uk" in soup.text
+
         assert len(mock_notification_service_calls) == 1
         assert mock_notification_service_calls[0].kwargs["personalisation"]["magic_link"] == AnyStringMatching(
             r"http://funding.communities.gov.localhost:8080/sign-in/.*"
@@ -69,19 +97,54 @@ class TestMagicLinkSignInView:
         ),
     )
     def test_post_valid_email_with_redirect(
-        self, anonymous_client, mock_notification_service_calls, db_session, next_, safe_next
+        self, anonymous_client, mock_notification_service_calls, factories, db_session, next_, safe_next
     ):
+        recipient_org = factories.organisation.create(can_manage_grants=False)
+        mhclg = _get_grant_managing_organisation()
+        grant = factories.grant.create(organisation=mhclg)
+        factories.grant_recipient.create(grant=grant, organisation=recipient_org)
+        user = factories.user.create(email="test@localgov.gov.uk")
+        factories.user_role.create(
+            user=user, organisation=recipient_org, grant=grant, permissions=[RoleEnum.DATA_PROVIDER]
+        )
+
         with anonymous_client.session_transaction() as session:
             session["next"] = next_
 
         response = anonymous_client.post(
             url_for("auth.request_a_link_to_sign_in"),
-            data={"email_address": "test@example.com"},
+            data={"email_address": user.email},
             follow_redirects=True,
         )
         assert response.status_code == 200
         assert (
             db_session.scalar(select(MagicLink).order_by(MagicLink.created_at_utc.desc())).redirect_to_path == safe_next
+        )
+
+        with anonymous_client.session_transaction() as session:
+            assert "next" not in session
+
+    def test_post_valid_email_with_no_next_redirects_to_route(
+        self, anonymous_client, mock_notification_service_calls, factories, db_session
+    ):
+        recipient_org = factories.organisation.create(can_manage_grants=False)
+        mhclg = _get_grant_managing_organisation()
+        grant = factories.grant.create(organisation=mhclg)
+        factories.grant_recipient.create(grant=grant, organisation=recipient_org)
+        user = factories.user.create(email="test@localgov.gov.uk")
+        factories.user_role.create(
+            user=user, organisation=recipient_org, grant=grant, permissions=[RoleEnum.DATA_PROVIDER]
+        )
+
+        response = anonymous_client.post(
+            url_for("auth.request_a_link_to_sign_in"),
+            data={"email_address": user.email},
+            follow_redirects=True,
+        )
+        assert response.status_code == 200
+        assert (
+            db_session.scalar(select(MagicLink).order_by(MagicLink.created_at_utc.desc())).redirect_to_path
+            == "/access/"
         )
 
         with anonymous_client.session_transaction() as session: