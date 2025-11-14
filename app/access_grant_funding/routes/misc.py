f9e7f2e399e24b6364e6a0ed46e3946564311c68
srh-sloan <sarah.sloan@communities.gov.uk>
2025-11-14 15:21:42 +0000

    WIP, needs a tidy up, new implementation with decorator and interfaces, not running checks

--- app/access_grant_funding/routes/misc.py
+++ app/access_grant_funding/routes/misc.py
@@ -4,8 +4,13 @@ from flask import abort, current_app, redirect, render_template, url_for
 from flask.typing import ResponseReturnValue
 
 from app.access_grant_funding.routes import access_grant_funding_blueprint
-from app.common.auth.decorators import access_grant_funding_login_required, is_access_org_member
+from app.common.auth.decorators import (
+    access_grant_funding_login_required,
+    has_grant_recipient_member_role,
+    is_access_org_member,
+)
 from app.common.data import interfaces
+from app.common.data.interfaces.collections import get_all_live_submissions_for_grant_recipient
 from app.common.data.interfaces.grants import get_grant
 from app.common.helpers.collections import SubmissionHelper
 
@@ -39,18 +44,31 @@ def list_grants(organisation_id: UUID) -> ResponseReturnValue:
 @access_grant_funding_blueprint.route(
     "/organisation/<uuid:organisation_id>/grant/<uuid:grant_id>/select-a-report", methods=["GET"]
 )
-@is_access_org_member
+@has_grant_recipient_member_role
 def list_reports(organisation_id: UUID, grant_id: UUID) -> ResponseReturnValue:
     grant = get_grant(grant_id=grant_id)
+
+    # TODO new get_grant_recipient by org and grant id
+    # don't need to check, interface will 404 if not found
+
     # find grant_recipient for this user-org-grant
-    grant_recipient = next((gr for gr in grant.grant_recipients if gr.organisation_id == organisation_id), None)
-    if not grant_recipient or interfaces.user.get_current_user() not in grant_recipient.users:
-        return render_template("access_grant_funding/errors/organisation_is_not_grant_recipient.html"), 404
+    # grant_recipient = next((gr for gr in grant.grant_recipients if gr.organisation_id == organisation_id), None)
+    # if not grant_recipient or interfaces.user.get_current_user() not in grant_recipient.users:
+    #     return render_template("access_grant_funding/errors/organisation_is_not_grant_recipient.html"), 404
+
+    # use updated get_all_submissions_with_mode_for_collection_with_full_schema with restrict param
+
+    grant_recipient = interfaces.user.current_user.get_grant_recipient(
+        organisation_id=organisation_id, grant_id=grant_id
+    )
+    if not grant_recipient:
+        return abort(404)
 
     submissions = [
         SubmissionHelper.load(submission.id)
-        for submission in grant_recipient.submissions
-        if submission.collection in grant.access_reports
+        for submission in get_all_live_submissions_for_grant_recipient(
+            grant_id=grant_id, grant_recipient_id=grant_recipient.id
+        )
     ]
     return render_template(
         "access_grant_funding/report_list.html",
