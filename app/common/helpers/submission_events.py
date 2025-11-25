20e2b71fc3d41a5a7513e2858eeaaf0ff6b0f245
Steven <steven.fountain@communities.gov.uk>
2025-11-25 19:26:44 +0000

    FSPT-1018: add events helper library
    Adds a small lib to represent submission events and their properties.
    
    By processed events in a reduced stream like this, either grouped by a
    form on the submission or the sumbission itself we should be able to
    flexibility model the workflow and audit requirements of submissions:
    - awaiting sign off (if certification required)
    - being signed off (if certification required)
    - being declined (if certification required)
    - being submitted
    
    Originally tried using pydantic but as the type validation didn't feel
    useful and the overhead in syntax to make it do something it didn't seem
    like it wanted to do did't feel worth it - uses native dataclasses
    instead.
    
    Note shallow copy is used when extracting metadata interfaces from
    submission event models as `asdict` creates a deep copy which means
    you'll end up with two ORM references which doesn't feel right.

---
+++ app/common/helpers/submission_events.py
@@ -0,0 +1,131 @@
+from dataclasses import asdict, dataclass, fields
+from datetime import datetime
+from typing import TYPE_CHECKING, Any, Protocol
+from uuid import UUID
+
+from app.common.data.types import SubmissionEventType
+
+if TYPE_CHECKING:
+    from _typeshed import DataclassInstance
+
+    from app.common.data.models import Submission, SubmissionEvent
+    from app.common.data.models_user import User
+
+
+@dataclass
+class CompletedMixin(Protocol):
+    is_completed: bool
+
+
+@dataclass
+class SignOffMixin(Protocol):
+    is_awaiting_sign_off: bool
+
+
+@dataclass
+class SubmittedMixin(Protocol):
+    is_submitted: bool
+
+
+@dataclass
+class FormCompletedEvent(CompletedMixin):
+    is_completed: bool = True
+
+
+@dataclass
+class FormResetToInProgressEvent(CompletedMixin):
+    is_completed: bool = False
+
+
+@dataclass
+class SubmissionSentForCertificationEvent(SignOffMixin):
+    is_awaiting_sign_off: bool = True
+
+
+@dataclass
+class SubmissionSubmittedEvent(SubmittedMixin, SignOffMixin):
+    is_submitted: bool = True
+    is_awaiting_sign_off: bool = False
+
+
+@dataclass
+class SentForCertificationMetadata:
+    sent_for_certification_by: "User | None" = None
+    sent_for_certification_at_utc: "datetime | None" = None
+
+
+@dataclass
+class SubmittedMetadata:
+    submitted_by: "User | None" = None
+    submitted_at_utc: datetime | None = None
+
+
+@dataclass
+class SubmissionState(SentForCertificationMetadata, SubmittedMetadata, SignOffMixin, SubmittedMixin):
+    is_awaiting_sign_off: bool = False
+    is_submitted: bool = False
+
+
+@dataclass
+class FormState(CompletedMixin):
+    is_completed: bool = False
+
+
+class SubmissionEventHelper:
+    def __init__(self, submission: "Submission"):
+        self.submission = submission
+
+    @property
+    def events(self) -> list["SubmissionEvent"]:
+        return sorted(self.submission.events, key=lambda x: x.created_at_utc, reverse=False)
+
+    def form_state(self, form_id: UUID) -> FormState:
+        return FormState(**self._reduce([e for e in self.events if e.target_key == form_id]))
+
+    @property
+    def submission_state(self) -> SubmissionState:
+        return SubmissionState(**self._reduce([e for e in self.events if e.target_key == self.submission.id]))
+
+    def _reduce(self, events: list["SubmissionEvent"]) -> dict[str, Any]:
+        state: dict[str, Any] = {}
+        for event in events:
+            state = state | event.data | self._extract_metadata(event)
+
+        return state
+
+    def _extract_metadata(self, event: "SubmissionEvent") -> dict[str, Any]:
+        match event.event_type:
+            case SubmissionEventType.SUBMISSION_SUBMITTED:
+                return shallow_asdict(
+                    SubmittedMetadata(
+                        submitted_by=event.created_by,
+                        submitted_at_utc=event.created_at_utc,
+                    )
+                )
+            case SubmissionEventType.SUBMISSION_SENT_FOR_CERTIFICATION:
+                return shallow_asdict(
+                    SentForCertificationMetadata(
+                        sent_for_certification_by=event.created_by,
+                        sent_for_certification_at_utc=event.created_at_utc,
+                    )
+                )
+            case _:
+                return {}
+
+    @staticmethod
+    def event_from(event_type: SubmissionEventType) -> dict[str, Any]:
+        match event_type:
+            case SubmissionEventType.FORM_RUNNER_FORM_COMPLETED:
+                return asdict(FormCompletedEvent())
+            case SubmissionEventType.FORM_RUNNER_FORM_RESET_TO_IN_PROGRESS:
+                return asdict(FormResetToInProgressEvent())
+            case SubmissionEventType.SUBMISSION_SUBMITTED:
+                return asdict(SubmissionSubmittedEvent())
+            case SubmissionEventType.SUBMISSION_SENT_FOR_CERTIFICATION:
+                return asdict(SubmissionSentForCertificationEvent())
+            case _:
+                raise NotImplementedError(f"No event class defined for event type {event_type}")
+
+
+def shallow_asdict(obj: "DataclassInstance") -> dict[str, Any]:
+    return {field.name: getattr(obj, field.name) for field in fields(obj)}
---
+++ tests/unit/common/helpers/test_submission_events.py
@@ -0,0 +1,127 @@
+import uuid
+from datetime import datetime
+
+from app.common.data.types import SubmissionEventType
+from app.common.helpers.submission_events import SubmissionEventHelper
+
+
+class TestSubmissionEventHelper:
+    class TestFormState:
+        def test_empty(self, factories):
+            submission = factories.submission.build()
+            events = SubmissionEventHelper(submission)
+            form_state = events.form_state(uuid.uuid4())
+            assert form_state.is_completed is False
+
+        def test_reduces(self, factories):
+            form = factories.form.build()
+            user = factories.user.build()
+            submission = factories.submission.build(collection=form.collection)
+            submission.events = [
+                factories.submission_event.build(
+                    event_type=SubmissionEventType.FORM_RUNNER_FORM_COMPLETED,
+                    target_key=form.id,
+                    created_by=user,
+                    data=SubmissionEventHelper.event_from(SubmissionEventType.FORM_RUNNER_FORM_COMPLETED),
+                ),
+            ]
+            events = SubmissionEventHelper(submission)
+            assert events.form_state(form.id).is_completed is True
+
+        def test_reduces_in_order(self, factories):
+            form = factories.form.build()
+            user = factories.user.build()
+            submission = factories.submission.build(collection=form.collection)
+            submission.events = [
+                factories.submission_event.build(
+                    event_type=SubmissionEventType.FORM_RUNNER_FORM_COMPLETED,
+                    target_key=form.id,
+                    created_by=user,
+                    data=SubmissionEventHelper.event_from(SubmissionEventType.FORM_RUNNER_FORM_COMPLETED),
+                    created_at_utc=datetime(2025, 11, 25, 0, 0, 0),
+                ),
+                factories.submission_event.build(
+                    event_type=SubmissionEventType.FORM_RUNNER_FORM_RESET_TO_IN_PROGRESS,
+                    target_key=form.id,
+                    created_by=user,
+                    data=SubmissionEventHelper.event_from(SubmissionEventType.FORM_RUNNER_FORM_RESET_TO_IN_PROGRESS),
+                    created_at_utc=datetime(2025, 11, 26, 0, 0, 0),
+                ),
+            ]
+            events = SubmissionEventHelper(submission)
+            assert events.form_state(form.id).is_completed is False
+
+            factories.submission_event.build(
+                submission=submission,
+                event_type=SubmissionEventType.FORM_RUNNER_FORM_COMPLETED,
+                target_key=form.id,
+                created_by=user,
+                data=SubmissionEventHelper.event_from(SubmissionEventType.FORM_RUNNER_FORM_COMPLETED),
+                created_at_utc=datetime(2025, 11, 27, 0, 0, 0),
+            )
+            assert events.form_state(form.id).is_completed is True
+
+    class TestSubmissionState:
+        def test_empty(self, factories):
+            submission = factories.submission.build()
+            events = SubmissionEventHelper(submission)
+            submission_state = events.submission_state
+            assert submission_state.is_submitted is False
+            assert submission_state.is_awaiting_sign_off is False
+            assert submission_state.submitted_by is None
+            assert submission_state.submitted_at_utc is None
+            assert submission_state.sent_for_certification_by is None
+            assert submission_state.sent_for_certification_at_utc is None
+
+        def test_reduces(self, factories):
+            form = factories.form.build()
+            user = factories.user.build()
+            submission = factories.submission.build(
+                collection=form.collection,
+            )
+            submission.events = [
+                factories.submission_event.build(
+                    event_type=SubmissionEventType.SUBMISSION_SENT_FOR_CERTIFICATION,
+                    target_key=submission.id,
+                    created_by=user,
+                    data=SubmissionEventHelper.event_from(SubmissionEventType.SUBMISSION_SENT_FOR_CERTIFICATION),
+                    created_at_utc=datetime(2025, 11, 25, 0, 0, 0),
+                )
+            ]
+            events = SubmissionEventHelper(submission)
+            submission_state = events.submission_state
+            assert submission_state.is_awaiting_sign_off is True
+            assert submission_state.sent_for_certification_by == user
+            assert submission_state.sent_for_certification_at_utc == datetime(2025, 11, 25, 0, 0, 0)
+            assert submission_state.is_submitted is False
+            assert submission_state.submitted_by is None
+
+        def test_reduces_in_order(self, factories):
+            form = factories.form.build()
+            user = factories.user.build()
+            submission = factories.submission.build(
+                collection=form.collection,
+            )
+            submission.events = [
+                factories.submission_event.build(
+                    event_type=SubmissionEventType.SUBMISSION_SENT_FOR_CERTIFICATION,
+                    target_key=submission.id,
+                    created_by=user,
+                    data=SubmissionEventHelper.event_from(SubmissionEventType.SUBMISSION_SENT_FOR_CERTIFICATION),
+                    created_at_utc=datetime(2025, 11, 24, 0, 0, 0),
+                ),
+                factories.submission_event.build(
+                    event_type=SubmissionEventType.SUBMISSION_SUBMITTED,
+                    target_key=submission.id,
+                    created_by=user,
+                    data=SubmissionEventHelper.event_from(SubmissionEventType.SUBMISSION_SUBMITTED),
+                    created_at_utc=datetime(2025, 11, 25, 0, 0, 0),
+                ),
+            ]
+            events = SubmissionEventHelper(submission)
+            assert events.submission_state.is_submitted is True
+            assert events.submission_state.is_awaiting_sign_off is False
+            assert events.submission_state.submitted_by == user
+            assert events.submission_state.submitted_at_utc == datetime(2025, 11, 25, 0, 0, 0)
+            assert events.submission_state.sent_for_certification_by == user
+            assert events.submission_state.sent_for_certification_at_utc == datetime(2025, 11, 24, 0, 0, 0)
