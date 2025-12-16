2ce3179f1a96aa19fc379ab3760666ed1a00f655
srh-sloan <sarah.sloan@communities.gov.uk>
2025-12-16 10:48:48 +0000

    WIP adding google analytics and cookie consent to access grant funding
    WIP moving JS out of html file
    
    WIP: basic consent mode

---
+++ app/assets/js/cookie-consent/index.js
@@ -0,0 +1,78 @@
+function gtag() {
+    window.dataLayer.push(arguments);
+}
+
+var googleTagLoaded = false;
+function loadGoogleTag() {
+    if (!googleTagLoaded) {
+        console.log("loading tag manager");
+        // Load Tag Manager script.
+        var gtmScript = document.createElement("script");
+        gtmScript.async = true;
+        gtmScript.src =
+            "https://www.googletagmanager.com/gtm.js?id=" + window.googleTagId;
+
+        var firstScript = document.getElementsByTagName("script")[0];
+        firstScript.parentNode.insertBefore(gtmScript, firstScript);
+        googleTagLoaded = true;
+    }
+}
+function initCookieConsent() {
+    window.dataLayer = window.dataLayer || [];
+    const cookie_banner = document.getElementById("cookie_banner");
+    const cookie_choice_msg = document.getElementById("cookie_choice_msg");
+    const cookies_rejected_msg = document.getElementById(
+        "cookies_rejected_msg",
+    );
+    const cookies_accepted_msg = document.getElementById(
+        "cookies_accepted_msg",
+    );
+    const accept_cookies_button = document.getElementById(
+        "btn_accept_analytics_cookies",
+    );
+    const reject_cookies_button = document.getElementById(
+        "btn_reject_analytics_cookies",
+    );
+    const btn_hide_cookies = document.getElementsByName("cookies[hide]");
+
+    accept_cookies_button.addEventListener("click", function () {
+        localStorage.setItem("consentGranted", "true");
+
+        gtag("consent", "update", {
+            ad_user_data: "denied",
+            ad_personalization: "denied",
+            ad_storage: "denied",
+            analytics_storage: "granted",
+        });
+        loadGoogleTag();
+        cookie_choice_msg.hidden = true;
+        cookies_accepted_msg.removeAttribute("hidden");
+    });
+    reject_cookies_button.addEventListener("click", function () {
+        localStorage.setItem("consentGranted", "false");
+        gtag("consent", "update", {
+            ad_user_data: "denied",
+            ad_personalization: "denied",
+            ad_storage: "denied",
+            analytics_storage: "denied",
+        });
+        cookie_choice_msg.hidden = true;
+        cookies_rejected_msg.removeAttribute("hidden");
+    });
+    btn_hide_cookies.forEach(function (value) {
+        value.addEventListener("click", function () {
+            cookie_banner.hidden = true;
+        });
+    });
+
+    // only loads the google tag manager if consent has been provided
+    // https://developers.google.com/tag-platform/security/concepts/consent-mode?#basic_consent_mode
+    if (localStorage.getItem("consentGranted") === "true") {
+        loadGoogleTag();
+    }
+    if (!localStorage.getItem("consentGranted")) {
+        cookie_banner.removeAttribute("hidden");
+        cookie_choice_msg.removeAttribute("hidden");
+    }
+}
+initCookieConsent();
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
                 "app/assets/main.scss",
                 "app/assets/main.js",
                 "app/assets/admin.scss",
+                "app/assets/js/cookie-consent/index.js",
             ],
             external: [
                 /assets\/fonts\/.*\.(woff|woff2)$/,
