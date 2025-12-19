9cc61238f280e93c76b0965ba66ab4bff25de76c
Steven <steven.fountain@communities.gov.uk>
2025-12-19 12:37:12 +0000

    BAU: cookie banner tests
    Make sure the cookie bannner is appropriately shown or hidden based on
    user choice and also make sure we're respecting the google tag manager
    "Basic consent" model by not loading the tag manager until consent is
    given - this should help us avoid regression on this.

--- tests/e2e/access_grant_funding/pages.py
+++ tests/e2e/access_grant_funding/pages.py
@@ -1,12 +1,32 @@
+from abc import ABC
+from typing import Self
+
 from playwright.sync_api import Page, expect
 
 from tests.e2e.pages import BasePage
 
 
-class RequestALinkToSignInPage(BasePage):
+class CookieBannerMixin(ABC):
+    domain: str
+    page: Page
+
+    def click_accept_cookies(self) -> Self:
+        self.page.get_by_role("button", name="Accept analytics cookies").click()
+        return self
+
+    def click_reject_cookies(self) -> Self:
+        self.page.get_by_role("button", name="Reject analytics cookies").click()
+        return self
+
+    def click_hide_cookies(self) -> Self:
+        self.page.get_by_role("button", name="Hide cookie message").click()
+        return self
+
+
+class RequestALinkToSignInPage(CookieBannerMixin, BasePage):
     def __init__(self, page: Page, domain: str) -> None:
         super().__init__(page, domain)
-        self.title = self.page.get_by_role("heading", name="Access grant funding")
+        self.title = self.page.get_by_role("heading", name="Access grant funding", exact=True)
         self.email_address = self.page.get_by_role("textbox", name="Email address")
         self.request_a_link = self.page.get_by_role("button", name="Request sign in link")
 
