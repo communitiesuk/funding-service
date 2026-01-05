a023e5fea0a41c918eb46e6d872163483ce3b3b8
srh-sloan <sarah.sloan@communities.gov.uk>
2026-01-05 14:08:38 +0000

    Tweaking test to work with updated access grant fundiing magic link journey
    Enabling running the magic link tests in dev and test

--- tests/e2e/access_grant_funding/test_magic_link_auth.py
+++ tests/e2e/access_grant_funding/test_magic_link_auth.py
@@ -8,7 +8,7 @@ from tests.e2e.config import EndToEndTestSecrets
 from tests.e2e.helpers import retrieve_magic_link
 
 
-@pytest.mark.skip_in_environments(["local", "dev", "test", "prod"])
+@pytest.mark.skip_in_environments(["prod"])
 def test_magic_link_redirect_journey(page: Page, domain: str, e2e_test_secrets: EndToEndTestSecrets):
     # Magic link page is no longer the default unauthenticated redirect so just go through that flow.
     request_a_link_page = RequestALinkToSignInPage(page, domain)
@@ -23,7 +23,7 @@ def test_magic_link_redirect_journey(page: Page, domain: str, e2e_test_secrets: 
     magic_link_url = retrieve_magic_link(notification_id, e2e_test_secrets)
     page.goto(magic_link_url)
 
-    expected_url_pattern = rf"^{domain}/organisation/[a-f0-9-]{{36}}/grants$"
+    expected_url_pattern = rf"^{domain}/access/organisation/[a-f0-9-]{{36}}/grants/[a-f0-9-]{{36}}/reports$"
 
     # JavaScript on the page automatically claims the link and should redirect to where they started.
     expect(page).to_have_url(re.compile(expected_url_pattern))
