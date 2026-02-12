6c2b9dc6de05e7eb50b1e9a77403cf0f7f3de03a
srh-sloan <sarah.sloan@communities.gov.uk>
2026-02-12 10:14:16 +0000

    Add and edit working

--- app/common/expressions/managed.py
+++ app/common/expressions/managed.py
@@ -1317,13 +1317,3 @@ class Custom(ManagedExpression):
     @staticmethod
     def update_validators(form: _ManagedExpressionForm) -> None:
         pass
-
-    @classmethod
-    def prepare_form_data(cls, add_context_data: AddContextToExpressionsModel) -> dict[str, Any]:
-        data = {
-            k: v
-            for k, v in add_context_data.expression_form_data.items()
-            if k != "add_context" and k != "remove_context"
-        }
-
-        return data
