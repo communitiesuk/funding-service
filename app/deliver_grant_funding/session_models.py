f894486aab3afc307942435586f349bf9ae3c75f
MarcUsher <99440441+MarcUsher@users.noreply.github.com>
2026-02-19 14:27:32 +0000

    FSPT-1146: Add first data set upload view
    Adds first step of CSV upload for data references, allowing you to pick
    the file (with some basic checks on the file), name it and categorise it
    
    Slight difference to the designs which I'll likely come back and fix up
    as the designs change.
    
    Currently uses the session to store the parsed csv/form data in the same
    way we do for adding context data. The slight issue here is that the,
    if the user uploaded a huge CSV, this would probably be larger than the
    max session size of 4mb. In the first instance the files will be
    relatively small and we're going to introduce S3 storage for the files
    in an upcoming PR which will mitigate this, as we can upload the file to
    s3 and then just track the s3 URI in the session rather than all the file
    data and reparse it on successful mapping and upload.

--- app/deliver_grant_funding/session_models.py
+++ app/deliver_grant_funding/session_models.py
@@ -3,7 +3,13 @@ from uuid import UUID
 
 from pydantic import BaseModel, ConfigDict
 
-from app.common.data.types import ExpressionType, ManagedExpressionsEnum, QuestionDataType
+from app.common.data.types import (
+    DataSourceType,
+    ExpressionType,
+    ManagedExpressionsEnum,
+    NumberTypeEnum,
+    QuestionDataType,
+)
 from app.common.expressions import ExpressionContext
 
 
@@ -90,3 +96,22 @@ class AddContextToExpressionsModel(BaseModel):
     data_source: ExpressionContext.ContextSources | None = None
     depends_on_question_id: UUID | None = None
     expression_id: UUID | None = None
+
+
+class DataSetColumnMapping(BaseModel):
+    column_name: str
+    data_type: Literal[QuestionDataType.TEXT_SINGLE_LINE, QuestionDataType.NUMBER]
+    number_type: NumberTypeEnum | None = None
+    prefix: str | None = None
+    suffix: str | None = None
+    max_decimal_places: int | None = None
+
+
+class DataSetUploadSessionModel(BaseModel):
+    name: str
+    data_source_type: DataSourceType
+    grant_recipient_identifier_columns: list[str] = []
+    data_columns: list[str]
+    preview_rows: list[dict[str, str]]
+    all_rows: list[dict[str, str]]
+    column_mappings: list[DataSetColumnMapping] = []
--- tests/unit/test_all_routes_access.py
+++ tests/unit/test_all_routes_access.py
@@ -68,6 +68,8 @@ routes_with_expected_grant_admin_only_access = [
     "deliver_grant_funding.edit_question_validation",
     "deliver_grant_funding.collection_configure_multiple_submissions",
     "deliver_grant_funding.set_guidance_for_multiple_submissions",
+    "deliver_grant_funding.upload_data_set",
+    "deliver_grant_funding.map_data_set_columns",
 ]
 routes_with_expected_collection_is_editable_decorator = [
     "deliver_grant_funding.change_report_name",
