7120e7199c77d57a27f210e8828a502e458b95ef
srh-sloan <sarah.sloan@communities.gov.uk>
2026-03-03 13:54:01 +0000

    Review comments: - Making ref data buttons appear in the textarea they relate to, not underneath
    - Removing the constraint to only allow one validation of a certain type against an individual question for custom validation expressions.
    This will allow form designers to chain together multiple custom expressions and require them all to pass.

---
+++ app/common/data/migrations/versions/050_multiple_custom_expressions.py
@@ -0,0 +1,60 @@
+"""Adjust the index on validation expressions to allow multiple custom validations per question.
+
+Revision ID: 050_multiple_custom_expressions
+Revises: 049_add_custom_expressions
+Create Date: 2026-02-09 12:47:40.332189
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+revision = "050_multiple_custom_expressions"
+down_revision = "049_add_custom_expressions"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.drop_index("uq_type_validation_unique_key", table_name="expression")
+    op.drop_index("uq_type_condition_unique_question", table_name="expression")
+    op.create_index(
+        "uq_type_validation_unique_key",
+        "expression",
+        ["type", "question_id", "managed_name"],
+        unique=True,
+        postgresql_where=(
+            "type = 'VALIDATION'::expression_type_enum AND managed_name IS NOT NULL "
+            "AND managed_name != 'CUSTOM'::managed_expression_enum"
+        ),
+    )
+
+    op.create_index(
+        "uq_type_condition_unique_question",
+        "expression",
+        ["type", "question_id", "managed_name", sa.literal_column("(context ->> 'question_id')")],
+        unique=True,
+        postgresql_where=(
+            "type = 'CONDITION'::expression_type_enum AND managed_name IS NOT NULL "
+            "AND managed_name != 'CUSTOM'::managed_expression_enum"
+        ),
+    )
+
+
+def downgrade() -> None:
+    op.drop_index("uq_type_validation_unique_key", table_name="expression")
+    op.drop_index("uq_type_condition_unique_question", table_name="expression")
+    op.create_index(
+        "uq_type_validation_unique_key",
+        "expression",
+        ["type", "question_id", "managed_name"],
+        unique=True,
+        postgresql_where="type = 'VALIDATION'::expression_type_enum",
+    )
+    op.create_index(
+        "uq_type_condition_unique_question",
+        "expression",
+        ["type", "question_id", "managed_name", sa.literal_column("(context ->> 'question_id')")],  # ty: ignore[invalid-argument-type]
+        unique=True,
+        postgresql_where="type = 'CONDITION'::expression_type_enum",
+    )
