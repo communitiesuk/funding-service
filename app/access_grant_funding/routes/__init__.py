0d01a9787a8f72f6453b95d7acb2977d7edfbfb9
Steven <steven.fountain@communities.gov.uk>
2026-05-26 19:01:19 +0100

    FSPT-1383: rename modules and templates

--- app/access_grant_funding/routes/__init__.py
+++ app/access_grant_funding/routes/__init__.py
@@ -4,7 +4,7 @@ access_grant_funding_blueprint = Blueprint(name="access_grant_funding", import_n
 
 
 from app.access_grant_funding.routes import (  # noqa: E402, F401
+    collections,
     misc,
-    reports,
     runner,
 )
