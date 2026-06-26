9b4006c095c0408c2c4eb06e49cf8cf74cb2d961
christianbentley96 <103260778+christianbentley96@users.noreply.github.com>
2026-06-26 10:39:34 +0100

    Update forms.py
    Updated description of checkbox to dynamically pull the maximum number of checkbox options from the config

--- app/deliver_grant_funding/forms.py
+++ app/deliver_grant_funding/forms.py
@@ -498,7 +498,7 @@ class QuestionForm(FlaskForm):
 
                 if question_type == QuestionDataType.CHECKBOXES:
                     self.data_source_items.description = (
-                        "Enter each option on a new line - you can add a maximum of 10 options"
+                        f"Enter each option on a new line - you can add a maximum of {current_app.config["MAX_DATA_SOURCE_ITEMS_CHECKBOXES"]} options"
                     )
 
             case QuestionDataType.TEXT_MULTI_LINE:
