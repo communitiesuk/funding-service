3539777f23d0da30f285848aa208d301a9f38494
srh-sloan <sarah.sloan@communities.gov.uk>
2026-06-26 07:11:54 +0100

    Updating the DataSetColumnMapping session model to consistently determine whether this mapping is british pounds, reducing the number of places we need to know what british pounds means. Also adding a helper method to convert an existing dataset column schema into a mapping object for use in validation, with tests

--- app/deliver_grant_funding/forms.py
+++ app/deliver_grant_funding/forms.py
@@ -1158,8 +1158,6 @@ class MapDataSetColumnsForm(FlaskForm):
                     mapping = DataSetColumnMapping(
                         column_name=column,
                         column_type=selected_value,
-                        prefix="£",
-                        max_decimal_places=2,
                     )
                 case "TEXT" | "INTEGER" | "DECIMAL" | _:
                     mapping = DataSetColumnMapping(
