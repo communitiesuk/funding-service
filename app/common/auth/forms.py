fa26a6230d1043bd20854faa73051848002fc88a
Albert Artur Kolozsvari <albert.kolozsvari@communities.gov.uk>
2026-08-05 22:30:38 +0100

    Feat: Update SignInForm form to not validate email address for public journey

--- app/common/auth/forms.py
+++ app/common/auth/forms.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from flask_wtf import FlaskForm
 from govuk_frontend_wtf.wtforms_widgets import GovSubmitInput, GovTextInput
 from wtforms import StringField, SubmitField
@@ -12,9 +14,15 @@ class SignInForm(FlaskForm):
         validators=[
             DataRequired(message="Enter your email address"),
             Email(message="Enter an email address in the correct format, like name@example.com"),
-            AccessGrantFundingEmail(),
         ],
         filters=[lambda x: x.strip() if x else x],
         widget=GovTextInput(),
     )
     submit = SubmitField("Request sign in link", widget=GovSubmitInput())
+
+    def __init__(self, *args: Any, is_public_sign_off: bool = False, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+        if not is_public_sign_off:
+            # If not public sign off, we expect the user to exist in the system
+            self.email_address.validators = [*self.email_address.validators, AccessGrantFundingEmail()]
