7af3403c20f96e2e5bece0fa9b73956f6000e232
Albert Artur Kolozsvari <albert.kolozsvari@communities.gov.uk>
2026-08-05 23:03:13 +0100

    Feat: Add public request a link page

--- app/common/auth/__init__.py
+++ app/common/auth/__init__.py
@@ -7,7 +7,7 @@ from flask.typing import ResponseReturnValue
 from flask_login import login_user, logout_user
 
 from app.common.auth.authorisation_helper import AuthorisationHelper
-from app.common.auth.decorators import redirect_if_authenticated
+from app.common.auth.decorators import redirect_if_authenticated, validate_public_sign_off
 from app.common.auth.forms import SignInForm
 from app.common.auth.sso import MSAL_ERROR_AUTHORIZATION_CODE_WAS_ALREADY_REDEEMED, build_auth_code_flow, build_msal_app
 from app.common.data import interfaces
@@ -21,6 +21,57 @@ auth_blueprint = Blueprint(
     __name__,
     url_prefix="/",
 )
+
+
+@auth_blueprint.route(
+    "/request-a-link-to-sign-in/<string:grant_slug>/<string:collection_slug>", methods=["GET", "POST"]
+)
+@validate_public_sign_off
+@redirect_if_authenticated
+@auto_commit_after_request
+def public_request_a_link_to_sign_in(grant_slug: str, collection_slug: str) -> ResponseReturnValue:
+    link_expired = request.args.get("link_expired", False)
+
+    grant = interfaces.grants.get_grant_by_slug(grant_slug)
+    collection = interfaces.collections.get_collection_by_slug(grant.id, collection_slug)
+
+    form = SignInForm(is_public_sign_off=True)
+    if form.validate_on_submit():
+        email = cast(str, form.email_address.data)
+
+        user = interfaces.user.get_user_by_email(email_address=email)
+
+        magic_link = interfaces.magic_link.create_magic_link(
+            user=user,
+            email=email,
+            # TODO: change redirect later
+            redirect_to_path=sanitise_redirect_url(url_for("access_grant_funding.index")),
+            collection=collection,
+        )
+
+        notification = notification_service.send_magic_link(
+            email,
+            magic_link_url=url_for("auth.claim_magic_link", magic_link_code=magic_link.code, _external=True),
+            magic_link_expires_at_utc=magic_link.expires_at_utc,
+            request_new_magic_link_url=url_for(
+                "auth.public_request_a_link_to_sign_in",
+                grant_slug=grant_slug,
+                collection_slug=collection_slug,
+                _external=True,
+            ),
+        )
+        session["magic_link_email_notification_id"] = notification.id
+        session["magic_link_requested"] = True
+
+        return redirect(url_for("auth.check_email", magic_link_id=magic_link.id))
+
+    return render_template(
+        "access_grant_funding/auth/public_sign_off_magic_link.html",
+        form=form,
+        link_expired=link_expired,
+        grant=grant,
+        collection=collection,
+    )
 
 
 @auth_blueprint.route("/request-a-link-to-sign-in", methods=["GET", "POST"])
@@ -76,7 +127,10 @@ def check_email(magic_link_id: uuid.UUID) -> ResponseReturnValue:
 
     notification_id = session.pop("magic_link_email_notification_id", None)
     return render_template(
-        "access_grant_funding/auth/check_email.html", email=magic_link.email, notification_id=notification_id
+        "access_grant_funding/auth/check_email.html",
+        email=magic_link.email,
+        notification_id=notification_id,
+        collection=magic_link.collection,
     )
 
 
@@ -88,6 +142,17 @@ def claim_magic_link(magic_link_code: str) -> ResponseReturnValue:
     if not magic_link or not magic_link.is_usable:
         if magic_link:
             session["next"] = magic_link.redirect_to_path
+
+            if magic_link.collection:
+                return redirect(
+                    url_for(
+                        "auth.public_request_a_link_to_sign_in",
+                        link_expired=True,
+                        grant_slug=magic_link.collection.grant.slug,
+                        collection_slug=magic_link.collection.slug,
+                    )
+                )
+
         return redirect(
             url_for(
                 "auth.request_a_link_to_sign_in",
