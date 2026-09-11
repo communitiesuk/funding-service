2c1969fa338b3774db0c0301a49c40504487c2d0
MarcUsher <99440441+MarcUsher@users.noreply.github.com>
2026-09-11 11:23:41 +0100

    FSPT-1614: Emit public sign up started metric
    Adds a log metric for the public sign up journey being started with
    a click on the 'Start now' button on a public sign up page.
    
    Gated to not emit metrics for non-authenticated Deliver grant funding
    users testing the Access grant funding journey.

--- app/access_grant_funding/routes/misc.py
+++ app/access_grant_funding/routes/misc.py
@@ -40,6 +40,7 @@ from app.common.helpers.collections import (
 from app.common.markdown import convert_text_to_govuk_markup
 from app.constants import SESSION_CREATE_ORGANISATION, SESSION_MATCHED_ORGANISATION
 from app.extensions import auto_commit_after_request, notification_service
+from app.metrics import MetricAttributeName, MetricEventName, emit_metric_count
 from app.types import FlashMessageType
 
 
@@ -325,6 +326,11 @@ def public_sign_up_start_page(grant_slug: str, collection_slug: str) -> Response
                 )
             )
 
+        emit_metric_count(
+            MetricEventName.PUBLIC_SIGN_UP_STARTED,
+            collection=collection,
+            custom_attributes={MetricAttributeName.SUBMISSION_MODE: str(SubmissionModeEnum.LIVE)},
+        )
         start_public_sign_up(collection.id)
         return redirect(
             url_for(
