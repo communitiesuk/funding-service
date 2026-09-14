b12785980cdc8242e30e660a536c85f69a9ed9c7
Albert Artur Kolozsvari <albert.kolozsvari@communities.gov.uk>
2026-09-14 09:55:31 +0100

    Add test

--- tests/integration/common/helpers/test_collections.py
+++ tests/integration/common/helpers/test_collections.py
@@ -94,6 +94,19 @@ class TestSubmissionHelper:
 
             assert forms == [eligibility_form, form_a, form_b]
 
+        def test_excludes_eligibility_form_when_not_requested(self, factories):
+            collection = factories.collection.create()
+            eligibility_form = factories.form.create(collection=collection, is_eligibility_section=True)
+            form_a = factories.form.create(collection=collection)
+            form_b = factories.form.create(collection=collection)
+            submission = factories.submission.create(collection=collection)
+
+            helper = SubmissionHelper(submission)
+            forms = helper.get_ordered_visible_forms(include_eligibility_forms=False)
+
+            assert forms == [form_a, form_b]
+            assert eligibility_form not in forms
+
     class TestAllVisibleQuestions:
         def test_excludes_eligibility_form_questions(self, factories):
             collection = factories.collection.create()
